607cec004e089020059ba4fc3c799acf215eb312
SahithiVyakaranam14 <sahithidivya14@gmail.com>
2025-02-19 17:18:16 +0530

    metro deployed finally

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,5 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx'];
 
 module.exports = config;
